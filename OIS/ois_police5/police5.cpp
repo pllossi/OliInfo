a3fb2896588f03a5b708922e9ffd4d6c2d40aae4
pcScuola <filippo.boschi.008@gmail.com>
2024-11-27 11:36:11 +0100

    2sheeeeesh

---
+++ OIS/ois_police5/police5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, T;
+vector<int> A, B, C, E;
+
+int main() {
+
+
+    cin >> N >> M >> T;
+    A.resize(M);
+    B.resize(M);
+    C.resize(M);
+    E.resize(M);
+    for (int i=0; i<M; i++)
+        cin >> A[i] >> B[i] >> C[i] >> E[i];
+    
+    return 0;
+}
