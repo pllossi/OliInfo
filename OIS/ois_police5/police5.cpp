9bc6af885dc1b7875ee0fe7ba859668c44b11205
pcScuola <filippo.boschi.008@gmail.com>
2024-11-30 08:23:36 +0100

    riapertuta es

--- OIS/ois_police5/police5.cpp
+++ OIS/ois_police5/police5.cpp
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
-=======
 // NOTE: it is recommended to use this even if you don't understand the following code.
 
->>>>>>> 9f98b71812f357b0622cc3b04d8f81ae6cf13a76
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-<<<<<<< HEAD
-=======
 // input data
->>>>>>> 9f98b71812f357b0622cc3b04d8f81ae6cf13a76
 int N, M, T;
 vector<int> A, B, C, E;
 
 int main() {
-<<<<<<< HEAD
-
-=======
 //  uncomment the following lines if you want to read/write from files
 //  ifstream cin("input.txt");
 //  ofstream cout("output.txt");
->>>>>>> 9f98b71812f357b0622cc3b04d8f81ae6cf13a76
 
     cin >> N >> M >> T;
     A.resize(M);
@@ -30,13 +22,9 @@ int main() {
     E.resize(M);
     for (int i=0; i<M; i++)
         cin >> A[i] >> B[i] >> C[i] >> E[i];
-<<<<<<< HEAD
-    
-=======
 
     // insert your code here
 
     cout << 42 << endl; // print the result
->>>>>>> 9f98b71812f357b0622cc3b04d8f81ae6cf13a76
     return 0;
 }
