50aa9ad5255edf60fa38eb5b8461e33bae44e817
pcScuola <filippo.boschi.008@gmail.com>
2024-12-07 09:47:34 +0100

    es treni non finito

--- OIS/preoii_treni/grader.cpp
+++ OIS/preoii_treni/grader.cpp
@@ -2,7 +2,43 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int tempo_massimo(int, int[], int[]);
+int tempo_massimo(int N, int a[], int b[])
+{
+    int maxtime = 0;
+    int lastimetaken=0;
+    int lastaken;//0 for a, 1 for b
+    for (int i = 0; i < N; i++){
+         if(a[i]>b[i]&&i==0){
+            maxtime+=a[i];
+            lastaken=0;
+            lastimetaken=i;
+         } else {
+            maxtime+=b[i];
+            lastaken=1;
+            lastimetaken=i;
+         }
+         if(a[i]>b[i]){
+            if(lastaken==0){
+                maxtime+=a[i];
+                lastimetaken=i;
+            } else {
+                if(lastimetaken==i-1)
+                    maxtime+=a[i];
+            }
+         } else {
+            if(lastaken==1){
+                maxtime+=b[i];
+                lastimetaken=i;
+            } else {
+                if(lastimetaken==i-1)
+                    maxtime+=b[i];
+                    lastimetaken=i;
+            }
+         }
+    }
+    return maxtime;
+}
+
 
 int main()
 {
--- OIS/preoii_treni/treni.cpp
+++ OIS/preoii_treni/treni.cpp
@@ -1,4 +1,14 @@
 int tempo_massimo(int N, int a[], int b[])
 {
-    return 42;
+    int maxtime = 0;
+    int lastaken=0;//0 for a, 1 for b
+    for (int i = 0; i < N, i++){
+         if(a[i]>b[i]){
+            if(lastaken==0)
+                maxtime+=a[i];
+         } else {
+            if(lastaken==1)
+                maxtime+=b[i];
+         }
+    }
 }
