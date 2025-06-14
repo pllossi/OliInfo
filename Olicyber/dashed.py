9bdc63a691d6aa4b563abb380d0bcb196bfca719
pllossi <115420435+pllossi@users.noreply.github.com>
2025-06-14 20:13:35 +0200

    Rimozione olicyber

--- Olicyber/dashed.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-base64_string = "c3ludHtDQUVTQVJfTUUhLWwwaF9UMDdfdkdfZTF0dUchfQo="
-
-decoded_bytes = base64.b64decode(base64_string)
-decoded_text = decoded_bytes.decode('utf-8')
-
-print(decoded_text)
