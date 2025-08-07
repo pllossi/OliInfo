57b61fe626540815e2a5e7fddeefc945f503e8ad
pllossi <115420435+pllossi@users.noreply.github.com>
2025-08-07 19:05:14 +0200

    Update poldo.cpp remove error

--- OIS/poldo/poldo.cpp
+++ OIS/poldo/poldo.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<int> panini(10000);
 vector<int> memo(10000);
 int N;
-int double_penetration(int pos,int ultimoPeso){
+int dp(int pos,int ultimoPeso){
         if(pos>=N){
                 return 0;
         }
@@ -16,10 +16,10 @@ int double_penetration(int pos,int ultimoPeso){
                         mangio=memo[pos];
                 } else 
                 {
-                        mangio=1+double_penetration(pos+1,panini[pos]);
+                        mangio=1+dp(pos+1,panini[pos]);
                 }
         }
-        nonMangio=double_penetration(pos+1,ultimoPeso);
+        nonMangio=dp(pos+1,ultimoPeso);
         memo[pos]=mangio;
         return max(mangio,nonMangio);
 }
@@ -33,7 +33,7 @@ int main()
         for(int i=0;i<N;i++){
                 in>>panini[i];
         }
-        double_penetration(0,10001);
+        dp(0,10001);
         out<<*max_element(memo.begin(),memo.end());
         return 0;
 }
